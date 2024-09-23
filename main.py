7dcc06ebb7bee3f0f906f9b4bd6f2773db03a65c
Dima-Go <dimitrygolde@gmail.com>
2024-09-23 20:56:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import datetime
 
-current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print(f"The current local time is: {current_time}")
-    
+def print_time():
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"The current local time is: {current_time}")
+print_time()
 
+def print_day_of_week():
+    current_day = datetime.datetime.now().strftime("%A")
+    print(f"Today is: {current_day}")
+print_day_of_week()
